03a0a892e34e5d7cc28555fe2375af43d35de19e
jneblina <janneblina7@gmail.com>
2023-02-15 16:57:34 -0800

    Diseño inicial del quiz

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,111 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key, required String title});
 
   @override
-  State<HomePage> createState() => _MyWidgetState();
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: QuizPage(),
+    );
+  }
 }
 
-class _MyWidgetState extends State<HomePage> {
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> puntuacion = [];
+
+  List<String> preguntas = [
+    '¿Los globulos rojos viven 4 meses?',
+    '¿El cuerpo humano adulto tiene 306 huesos?',
+    '¿La cobalamina es una vitamina?',
+  ];
+
+  int numeroPregunta = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xff034732),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    preguntas[numeroPregunta],
+                    style: const TextStyle(color: Colors.white, fontSize: 26),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          numeroPregunta++;
+                          puntuacion.add(const Icon(
+                            Icons.check,
+                            color: Colors.green,
+                          ));
+                        });
+                      },
+                      child: const Text(
+                        'Verdadero',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          numeroPregunta++;
+                          puntuacion.add(const Icon(
+                            Icons.close,
+                            color: Colors.red,
+                          ));
+                        });
+                      },
+                      child: const Text(
+                        'Falso',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                ),
+              ),
+            ),
+            Row(
+              children: puntuacion,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
